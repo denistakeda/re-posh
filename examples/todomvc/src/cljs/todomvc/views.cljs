a6b69b5ef6677625b868fa05bb6791ef093e43fa
Denis Krivosheev <denis.takeda@gmail.com>
2017-11-07 09:47:51 +0300

    feat(create-todo-button): implement create todo button
    Implement create button and handle to create a new entity
    
    Closes #15

--- examples/todomvc/src/cljs/todomvc/views.cljs
+++ examples/todomvc/src/cljs/todomvc/views.cljs
@@ -1,12 +1,6 @@
 (ns todomvc.views
   (:require [re-frame.core :as re-frame]))
 
-(defn main-panel []
-  [:div {:class-name "main-panel"}
-   [:h1 "TodoMVC"]
-   [create-task-panel]
-   [task-list]])
-
 (defn create-task-panel []
   (let [form-id (re-frame/subscribe [:create-todo-form/id])
         form (re-frame/subscribe [:create-todo-form @form-id])]
@@ -14,14 +8,9 @@
       [:div {:class-name "create-task-panel"}
        [:input {:type "text"
                 :value (:create-todo-form/title @form)
-                :on-change #(re-frame/dispatch [:create-todo-form/set-title @form-id (-> % .-target .-value)])}]])))
-
-(defn task-list []
-  (let [task-ids (re-frame/subscribe [:task-ids])]
-    (fn []
-      [:div {:class-name "task-list"}
-       (for [task-id @task-ids]
-         ^{:key task-id} [task-list-item task-id])])))
+                :on-change #(re-frame/dispatch [:create-todo-form/set-title @form-id (-> % .-target .-value)])}]
+       [:button {:class-name "create-task-button"
+                 :on-click   #(re-frame/dispatch [:create-todo-form/create-todo @form-id (:create-todo-form/title @form)])} "Create"]])))
 
 (defn task-list-item [id]
   (let [task (re-frame/subscribe [:task id])]
@@ -31,3 +20,16 @@
                 :checked (:task/done? @task)
                 :on-change #(re-frame/dispatch [:task/set-status (:db/id @task) (not (:task/done? @task))])}]
        [:span (:task/title @task)]])))
+
+(defn task-list []
+  (let [task-ids (re-frame/subscribe [:task-ids])]
+    (fn []
+      [:div {:class-name "task-list"}
+       (for [task-id @task-ids]
+         ^{:key task-id} [task-list-item task-id])])))
+
+(defn main-panel []
+  [:div {:class-name "main-panel"}
+   [:h1 "TodoMVC"]
+   [create-task-panel]
+   [task-list]])
